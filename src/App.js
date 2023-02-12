e08e491b53396778776c1fad16b120cb34bc5086
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-02-12 20:52:55 +0530

    Mini Calculator

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Calculator from "./components/Calculator";
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Mini Calculator</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
